ce3169f59627fe06dad6293d647178302b01cffd
Mohit Kaninwal <mohit37.my@gmail.com>
2025-07-17 00:35:09 +0530

    Folder structure  updated

--- template.py
+++ template.py
@@ -15,9 +15,25 @@ list_of_files=[
     'src/prompt.py',
     '.env',
     'setup.py',
-    'research/trials.py,'
+    'research/trials.ipynb',
     'app.py',
     'store_index.py',
-    'static',
+    'static/.gitkeep',
     'templates/chat.html'
 ]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir, filename= os.path.split(filepath)
+
+    if filedir !='' :
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f'creating directory; {filedir} for the file {filename}')
+    
+    if(not os.path.exists(filepath)) or (os.path.getsize(filename)==0):
+        with open(filepath, 'w') as f:
+            pass 
+            logging.info(f'creating empty file: {filepath}')
+
+    else:
+        logging.info(f'{filename} is already created')
